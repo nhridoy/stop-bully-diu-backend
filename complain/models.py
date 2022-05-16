aec533e7a6f9ab435d417ecd9cf99287fb8dcc6d
unknown <gmzulkar@gmail.com>
2022-05-16 23:38:52 +0600

    field remove

--- complain/models.py
+++ complain/models.py
@@ -11,9 +11,8 @@ status = (
 
 class ComplainModel(models.Model):
     user = models.ForeignKey(user_model.User, on_delete=models.CASCADE, related_name='complain_user')
-    location = models.CharField(max_length=255, blank=True)
+    title = models.CharField(max_length=255, blank=True)
     suspect = models.CharField(max_length=255, blank=True)
-    suspect_description = models.CharField(max_length=255, blank=True)
     details = models.TextField()
     attachments = models.FileField(blank=True, null=True)
     status = models.CharField(max_length=100, choices=status)
