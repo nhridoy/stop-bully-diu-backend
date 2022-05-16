52a91a1c2747b0db272a033beecc40e799012324
unknown <gmzulkar@gmail.com>
2022-05-16 23:28:58 +0600

    suspect info added

--- complain/models.py
+++ complain/models.py
@@ -12,6 +12,8 @@ status = (
 class ComplainModel(models.Model):
     user = models.ForeignKey(user_model.User, on_delete=models.CASCADE, related_name='complain_user')
     location = models.CharField(max_length=255, blank=True)
+    suspect = models.CharField(max_length=255, blank=True)
+    suspect_description = models.CharField(max_length=255, blank=True)
     details = models.TextField()
     attachments = models.FileField(blank=True, null=True)
     status = models.CharField(max_length=100, choices=status)
